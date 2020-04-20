179867620fbae8e987407700aa13976a8ff82c35
karmakar4297 <62100654+karmakar4297@users.noreply.github.com>
2020-04-21 00:08:06 +0530

    Update Prob2_Euler.py

--- Prob2_Euler.py
+++ Prob2_Euler.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#n=int(input('Enter the number of steps: '))
 def dydx(y,x):
     return (y/x-(y/x)**2)
 h=0.1
@@ -10,13 +9,16 @@ w=np.zeros(n, dtype=float)
 y_true=np.zeros(n, dtype=float)
 for i in range(0, n):
     y_true[i]=t[i]/(1+np.log(t[i]))
-    #print('In loop y_true \n loop no: ',i+1,'\t t[i]=',t[i],'\t np.log=',np.log(t[i]))
 w[0]=1
 for i in range(0,n-1):
     w[i+1]=w[i]+h*dydx(w[i],t[i])
-#print(t)
-#print(y_true)
-#print(w)
+abserr=np.zeros(n, dtype=float)
+relerr=np.zeros(n, dtype=float)
+print('t\t\ty\t\ty true\t\tabs err\t\trel err\n')
+for i in range(0,n):
+    abserr[i]=abs(w[i]-y_true[i])
+    relerr[i]=abs((w[i]-y_true[i])/w[i])
+    print("{:.3f}".format(t[i]),'\t\t',"{:.3f}".format(w[i]),'\t\t',"{:.3f}".format(y_true[i]),'\t\t',"{:.3f}".format(abserr[i]),'\t\t',"{:.3f}".format(relerr[i]),'\n')
 plt.plot(t,w,color='blue', label='Euler-Backward')
 plt.plot(t,y_true, color='green', label='True solution')
 plt.xlabel('x')
